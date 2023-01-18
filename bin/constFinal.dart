5bb80466adb94f8abf244fdc47242cfb74cca21a
Ridwan Ibrahim <ridwanmiharbi@gmail.com>
2023-01-18 19:46:26 +0700

    const and final

---
+++ bin/constFinal.dart
@@ -0,0 +1,9 @@
+void main() {
+  const name = 'Ridwan';
+  final firstName = 'Ridwan';
+
+  /* error karena variable const dan final tidak bisa dirubah
+  name = 'Ibrahim'; 
+  firstName = 'Ibrahim';
+  */
+}
